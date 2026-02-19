2da2db9d55c927393bf440dbdcae3ff89cfab5a6
MrXrono <mrxrono@users.noreply.github.com>
2026-02-19 18:20:26 +0000

    revert: rollback remote file browser functionality changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.nanored.vpn"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4000040
-        versionName = "1.6.0.14"
+        versionCode = 4000035
+        versionName = "1.6.0.9"
         multiDexEnabled = true
 
         splits {
